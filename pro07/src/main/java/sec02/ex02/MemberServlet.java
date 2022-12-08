153c592e3c4df2166999c3d9d015d8fd9d37f391
MyeongHyeonYoo <victorea5@naver.com>
2022-12-08 20:32:44 +0900

    add file

--- pro07/src/main/java/sec02/ex02/MemberServlet.java
+++ pro07/src/main/java/sec02/ex02/MemberServlet.java
@@ -45,7 +45,7 @@ public class MemberServlet extends HttpServlet {
 			dao.delMember(id);
 		}
 		
-		List list = dao.listMembers();
+		List<MemberVO> list = dao.listMembers();
 		out.print("<html><body>");
 		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
 		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td >삭제</td></tr>");
